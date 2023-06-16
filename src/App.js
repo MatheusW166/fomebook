f6aa843ed4ff49cf9378bf3c40b584c79acec3fb
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-06-16 16:03:31 -0300

    ref: global style definition moved to index

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import ResetStyles from "./styles/reset.styles.js";
-import GlobalStyles from "./styles/global.styles.js";
 import { BrowserRouter } from "react-router-dom";
 import MyRoutes from "./routes.js";
 import UserProvider from "./context/user.context.js";
 
 function App() {
   return (
-    <>
-      <ResetStyles />
-      <GlobalStyles />
-      <UserProvider>
-        <BrowserRouter>
-          <MyRoutes />
-        </BrowserRouter>
-      </UserProvider>
-    </>
+    <UserProvider>
+      <BrowserRouter>
+        <MyRoutes />
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ResetStyles from "./styles/reset.styles";
+import GlobalStyles from "./styles/global.styles";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ResetStyles />
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );

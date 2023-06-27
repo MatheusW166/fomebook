96ef27133bf163491d224e12c347ca835bca1938
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-06-27 18:22:05 -0300

    feat: query client provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import MyRoutes from "./routes.js";
 import UserProvider from "./context/user.context.js";
+import { QueryCache, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryCache();
 
 function App() {
-  return (
-    <UserProvider>
-      <BrowserRouter>
-        <MyRoutes />
-      </BrowserRouter>
-    </UserProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<UserProvider>
+				<BrowserRouter>
+					<MyRoutes />
+				</BrowserRouter>
+			</UserProvider>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
